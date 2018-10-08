a2546940b95c91a4575b943eb1703b6ca047b093
Maria De La Osa Martinez <mdelaosa@beta10.aulas.gsyc.urjc.es>
2018-10-08 17:43:39 +0200

    Importamos clase y añadimos error

---
+++ karaoke.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 16:53:07 2018
+
+@author: mdelaosa
+"""
+import sys
+import smallsmilhandler
+from xml.sax import make_parser
+
+
+
+
+if __name__ == '__main__':
+    
+    
+    if len(sys.argv) != 2:
+            print ("Usage: python3 karaoke.py file.smil.")
+    else:
+        karaokesmill = smallsmilhandler.SmallSMILHandler()
+        parser = make_parser()
+        parser.setContentHandler(karaokesmill)
+        parser.parse(open(sys.argv[1]))
+        data = karaokesmill.get_tags()
+        print(data)
