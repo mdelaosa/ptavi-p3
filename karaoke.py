30988c56e2bcfe5c1f47227104ec59d37ea2b278
Maria De La Osa Martinez <mdelaosa@alpha33.aulas.gsyc.urjc.es>
2018-10-10 13:34:40 +0200

    Karaoke punto 4

--- karaoke.py
+++ karaoke.py
@@ -9,18 +9,19 @@ import sys
 import smallsmilhandler
 from xml.sax import make_parser
 
-
-
-
 if __name__ == '__main__':
-    
-    
+
     if len(sys.argv) != 2:
-            print ("Usage: python3 karaoke.py file.smil.")
+            print("Usage: python3 karaoke.py file.smil.")
     else:
         karaokesmill = smallsmilhandler.SmallSMILHandler()
         parser = make_parser()
         parser.setContentHandler(karaokesmill)
         parser.parse(open(sys.argv[1]))
         data = karaokesmill.get_tags()
-        print(data)
+
+        for atributo in data:
+            for datos in atributo:
+                if atributo[datos] != '':
+                    print(datos, '=', atributo[datos], end='\t')
+            print(end='\n')
