1f2d3bd5e26f120605b0ba8af959038be04e5bbc
Maria De La Osa Martinez <mdelaosa@gamma18.aulas.gsyc.urjc.es>
2018-10-15 10:37:48 +0200

    Karaoke + json + local

--- karaoke.py
+++ karaoke.py
@@ -12,29 +12,29 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
             print("Usage: python3 karaoke.py file.smil.")
     else:
+        file = sys.argv[1]
         karaokesmill = smallsmilhandler.SmallSMILHandler()
         parser = make_parser()
         parser.setContentHandler(karaokesmill)
-        parser.parse(open(sys.argv[1]))
+        parser.parse(open(file))
         data = karaokesmill.get_tags()
 
-        fichsmil = sys.argv[1]
         fichjson = ''
         if fichjson == '':
-            fichjson = fichsmil.replace('.smil', '.json')
+            fichjson = file.replace('.smil', '.json')
 
         with open(fichjson, 'w') as cambiojson:
             json.dump(data, cambiojson, indent=3)
 
         for datos in data:
             for atributo, valor in datos.items():
-                if atributo == 'name':
-                    print(valor, end='\t')
                 if atributo == 'src':
                     if valor.startswith('http://'):
-                        archivo = valor.split('/')
-                        descarga = urllib.request.urlretrieve(valor, archivo)
-                        valor = descarga
+                        archivo = valor.split('/')[-1]
+                        urllib.request.urlretrieve(valor)
+                        valor = archivo
+                if atributo == 'name':
+                    print(valor, end='\t')
                 if valor != '' and atributo != 'name':
                     print(atributo, "=", "'", valor, "'", end='\t')
             print(end='\n')
