f519b3b3d21d219ddd4b3ca8e9e9ba91fbb3d95a
Maria De La Osa Martinez <mdelaosa@alpha48.aulas.gsyc.urjc.es>
2018-10-15 17:53:14 +0200

    Karaoke con KaraokeLocal

--- karaoke.py
+++ karaoke.py
@@ -7,34 +7,57 @@ import smallsmilhandler
 from xml.sax import make_parser
 
 
-if __name__ == '__main__':
+class KaraokeLocal:
 
-    if len(sys.argv) != 2:
-            print("Usage: python3 karaoke.py file.smil.")
-    else:
-        file = sys.argv[1]
-        karaokesmill = smallsmilhandler.SmallSMILHandler()
-        parser = make_parser()
-        parser.setContentHandler(karaokesmill)
-        parser.parse(open(file))
-        data = karaokesmill.get_tags()
+    def __init__(self, file):
+        try:
+            karaokesmill = smallsmilhandler.SmallSMILHandler()
+            parser = make_parser()
+            parser.setContentHandler(karaokesmill)
+            parser.parse(open(file))
+            self.data = karaokesmill.get_tags()
 
-        fichjson = ''
-        if fichjson == '':
-            fichjson = file.replace('.smil', '.json')
+        except FileNotFoundError:
+            sys.exit("Usage: python3 karaoke.py file.smil.")
 
-        with open(fichjson, 'w') as cambiojson:
-            json.dump(data, cambiojson, indent=3)
-
-        for datos in data:
+    def __str__(self):
+        for datos in self.data:
             for atributo, valor in datos.items():
                 if atributo == 'src':
                     if valor.startswith('http://'):
                         url = valor.split('/')[-1]
-                        urllib.request.urlretrieve(valor)
                         valor = url
                 if atributo == 'name':
-                    print(valor, end='\t')
-                if valor != '' and atributo != 'name':
-                    print(atributo, "=", "'", valor, "'", end='\t')
-            print(end='\n')
+                    resultado = valor
+                elif valor != '' and atributo != 'name':
+                    resultado += '\t' + atributo + "= '" + valor + "'"
+            return resultado
+
+    def do_local(self):
+        for datos in self.data:
+            for atributo, valor in datos.items():
+                if atributo == 'src':
+                    if valor.startswith('http://'):
+                        url = valor.split('/')[-1]
+                        urllib.request.urlretrieve(valor, url)
+                        valor = url
+
+    def to_json(self, file, fichjson=''):
+        if fichjson == '':
+            fichjson = file.replace('.smil', '.json')
+
+        with open(fichjson, 'w') as to_json:
+            json.dump(self.data, to_json, indent=3)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("file.smil is not found")
+    else:
+        file = sys.argv[1]
+        karaoke = KaraokeLocal(file)
+        print(karaoke.__str__())
+        karaoke.to_json(file)
+        karaoke.do_local()
+        karaoke.to_json(file, 'local.json')
+        print(karaoke)
