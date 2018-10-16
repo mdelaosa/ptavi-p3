fb936a7bb37d6d34452d52f412194e8ef38d4e4b
Maria De La Osa Martinez <mdelaosa@beta10.aulas.gsyc.urjc.es>
2018-10-16 15:10:44 +0200

    Small SMIL Handler

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -13,40 +13,39 @@ class SmallSMILHandler(ContentHandler):
 
     def startElement(self, name, attrs):
         if name == "root-layout":
-            self.width = attrs.get("width", "")
-            self.height = attrs.get("height", "")
-            self.background_color = attrs.get("background-color", "")
-            self.data.append({"name": name, "width": self.width,
-                              "height": self.height,
-                              "background-color": self.background_color})
+            root_attrs = {'name': name}
+            for attr in ['width', 'height', 'background_color']:
+                if attr in attrs:
+                    root_attrs[attr] = attrs[attr]
+            self.data.append(root_attrs)
+
         elif name == "region":
-            self.id = attrs.get("id", "")
-            self.top = attrs.get("top", "")
-            self.bottom = attrs.get("bottom", "")
-            self.left = attrs.get("left", "")
-            self.right = attrs.get("right", "")
-            self.data.append({"name": name, "id": self.id, "top": self.top,
-                              "bottom": self.bottom, "left": self.left,
-                              "right": self.right})
+            region_attrs = {'name': name}
+            for attr in ['id', 'top', 'bottom', 'left', 'right']:
+                if attr in attrs:
+                    region_attrs[attr] = attrs[attr]
+            self.data.append(region_attrs)
+
         elif name == "img":
-            self.src = attrs.get("src", "")
-            self.region = attrs.get("region", "")
-            self.begin = attrs.get("begin", "")
-            self.dur = attrs.get("dur")
-            self.data.append({"name": name, "src": self.src,
-                              "region": self.region, "begin": self.begin,
-                              "dur": self.dur})
+            img_attrs = {'name': name}
+            for attr in ['src', 'region', 'begin', 'dur']:
+                if attr in attrs:
+                    img_attrs[attr] = attrs[attr]
+            self.data.append(img_attrs)
+
         elif name == "audio":
-            self.src = attrs.get("src", "")
-            self.begin = attrs.get("begin", "")
-            self.dur = attrs.get("dur", "")
-            self.data.append({"name": name, "src": self.src,
-                              "begin": self.begin, "dur": self.dur})
+            audio_attrs = {'name': name}
+            for attr in ['src', 'begin', 'dur']:
+                if attr in attrs:
+                    audio_attrs[attr] = attrs[attr]
+            self.data.append(audio_attrs)
+
         elif name == "textstream":
-            self.src = attrs.get("src", "")
-            self.region = attrs.get("region", "")
-            self.data.append({"name": name, "src": self.src,
-                              "region": self.region})
+            text_attrs = {'name': name}
+            for attr in ['src', 'region']:
+                if attr in attrs:
+                    text_attrs[attr] = attrs[attr]
+            self.data.append(text_attrs)
 
     def get_tags(self):
         return self.data
